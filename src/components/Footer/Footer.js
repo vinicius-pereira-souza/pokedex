6175f13461b88ec33f75293af923d83173b09994
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2022-08-11 15:42:25 -0300

    validação do input vompleto

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import style from './Footer.module.css'
 function Footer() {
   return (
     <div className={style.footerContainer}>
-      <p>Feito por Vinicius <a href="https://github.com/vinicius-pereira-souza">Link para o GitHub</a></p>
+      <p><a href="https://github.com/vinicius-pereira-souza">Link para o GitHub</a></p>
     </div>
   )
 }
